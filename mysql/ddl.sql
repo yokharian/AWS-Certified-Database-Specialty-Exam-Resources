56aeab5a5cf79911ec14a4916eb21a3a9b1092fd
Sofia Escobedo <63127299+yokharian@users.noreply.github.com>
2023-02-14 20:40:01 -0600

    Create ddl.sql

---
+++ mysql/ddl.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `aws_costs` 
+(
+    `Group hierarchy`	VARCHAR(512),
+    `Region`	VARCHAR(512),
+    `Description`	VARCHAR(512),
+    `Service`	VARCHAR(512),
+    `Upfront`	VARCHAR(512),
+    `Monthly`	VARCHAR(512),
+    `First 12 months total`	DOUBLE,
+    `Currency`	VARCHAR(512),
+    `Configuration summary`	VARCHAR(512)
+);
